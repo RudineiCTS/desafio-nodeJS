c7c89b2a1ed2ddf5fdcab9c27c025cb00db31466
Rudinei Costa <costaaugusto177@outlook.com>
2020-08-25 17:58:10 -0300

    desafio-node-OK

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs}= request.body;
+ 
+  const repository ={
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const {id}= request.params;
+    const {title, url, techs}= request.body;
+
+    const repositoryIndex = repositories.findIndex(project => project.id === id)
+
+    if(repositoryIndex < 0){
+      return response.status(400).json({message: 'repository not '})
+    }
+    
+    const repository = {
+      ...repositories[repositoryIndex],
+      title,
+      url,
+      techs,
+    }
+
+    repositories[repositoryIndex]= repository;
+
+    return response.status(200).json(repository);
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    const { id }= request.params;
+
+    const repositoryIndex = repositories.findIndex(project => project.id === id)
+
+    if(repositoryIndex < 0){
+      return response.status(400).json({message:"repository is not defined"});
+
+    }
+
+    repositories.splice(repositoryIndex, 1);
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id }=request.params;
+  
+  const repositoryIndex= repositories.findIndex(project => project.id === id);
+  if(repositoryIndex < 0){
+    return response.status(400).json({erro: 'repository does not exist'});
+
+  }
+  repositories[repositoryIndex].likes += 1;
+
+  return response.json(repositories[repositoryIndex]);
+
 });
 
 module.exports = app;
